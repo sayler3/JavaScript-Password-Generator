8ada3159681fc2bd0b16d7b317347a700058503e
Sam Ayler <sayler8701@gmail.com>
2020-10-19 16:38:33 -0700

    adding array for uppercase letters

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate password from the function 
 function generatePassword() {
 
   var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var sp = [ "~", "@", "!", "#", "$", "%", "^", "&", "*", "<", ">", "?", "_", "-"];
   var chars = ["a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","q","w","e","r","t","y","u","i","o","p"];
-
+  var charUp = ["A", "S", "D","F","G","H","J","K","L","Z","X","C","V","B","N","M","Q","W","E","R","T","Y","U","I","O","P"];
+ 
   var possibles = [];
 
   var length = parseInt(prompt("How many characters would you like in your password?"));
@@ -20,18 +22,19 @@ function generatePassword() {
     numTrue = confirm("Would you like number?");
     spTrue = confirm("Would you like special charaters?");
     charsTrue = confirm("Would you like lowercase letters?");
-    //chars.toUpperCaseTrue = confirm("Would you like uppercase letters?");
+    charUpTrue = confirm("Would you like uppercase letters?");
     
     if (numTrue) { possibles.push(num)}
     if (spTrue) { possibles.push(sp)}
     if (charsTrue) { possibles.push(chars)}
-    //if (chars.toUpperCaseTrue) { possibles.push(chars.toUpperCase)}
-  }
+    if (charUpTrue) { possibles.push(charUp)}
 
+  }
+  
   for (let i = 0; i < possibles.length; i++) {
     let ran = Math.floor(Math.random() * possibles[i].length)
-    //console.log(possibles[i][ran]);
     password += possibles[i][ran];
+
   }
   console.log(password);
 }
@@ -42,7 +45,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = toString(password);
 
   }
 
